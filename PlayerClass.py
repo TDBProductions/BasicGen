f7434ee2bee8c2536c740d293a9331e8a5ac96aa
cousey101 <cousey101@gmail.com>
2015-12-19 20:31:32 -0500

    Creating PlayerClass.py
    Housing all the player class information

---
+++ PlayerClass.py
@@ -0,0 +1,29 @@
+class PlayerClass:
+    self.__init__(self, StatArray)
+        self.STR = StatArray[0]
+        self.DEX = StatArray[1]
+        self.INT = StatArray[2]
+        self.CON = StatArray[3]
+        self.WIS = StatArray[4]
+        self.CHA = StatArray[5]
+
+class Fighter(PlayerClass):
+    pass
+
+class Thief(PlayerClass):
+    pass
+
+class Cleric(PlayerClass):
+    pass
+
+class MagicUser(PlayerClass):
+    pass
+
+class Elf(PlayerClass):
+    pass
+
+class Dwarf(PlayerClass):
+    pass
+
+class Halfling(PlayerClass):
+    pass
